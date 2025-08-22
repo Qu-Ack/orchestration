2aafa01bb842ac922739020028eb33b763ac23a3
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2025-08-22 20:59:14 +0530

    (BUG): HOST

--- sql/schema/001_deployments.sql
+++ sql/schema/001_deployments.sql
@@ -1,12 +1,3 @@
 -- +goose Up
-CREATE TABLE deployments (
-    id VARCHAR PRIMARY KEY,
-    clone_url VARCHAR NOT NULL,
-    branch VARCHAR NOT NULL,
-    repo_name VARCHAR NOT NULL
-);
 
 -- +goose Down
-DROP TABLE deployments;
-
-
